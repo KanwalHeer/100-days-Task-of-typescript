a1163d0c07a8e25547c3d9e71afd0af86b939e3a
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-03-18 17:04:04 +0500

    Add files via upload

---
+++ 17day.ts
@@ -0,0 +1,30 @@
+//Question :49;
+function RestPera(arr:string[]) {
+    arr.forEach(hobby => {
+        console.log(`I enjoy ${hobby}`)
+    });
+}
+let myHobbies = ["coding","raading","sleeping"]
+//RestPera(myHobbies)
+
+//Question : 50;
+ const myIdealDayIs = `My ideal day whould involes,
+ 1: combine study with friends.
+ 2: work on my personal project.
+ 3: spend time wioth family.`
+ //console.log(myIdealDayIs)
+
+ //Question :51;
+ function simpleFunction(hight:number,width:number):number {
+    let area = hight * width
+    return area
+ }
+ let AreaIs = simpleFunction(20,3)
+ console.log(AreaIs)
+
+const arrowFunction  = (hight:number,width:number):number=>{
+    let area = hight * width
+    return area
+}
+let newArea = arrowFunction(20,3)
+console.log(newArea)
