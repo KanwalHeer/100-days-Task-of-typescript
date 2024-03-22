01f63a7567a8ed65429942924a20275ae4ee0f4d
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-03-22 15:29:40 -0700

    Update 22day.ts

--- 22day.ts
+++ 22day.ts
@@ -3,15 +3,15 @@
 function mergeNumberString(strings:string,numbers:number) {
     return strings + numbers
 }
-//console.log(mergeNumberString("Age:",22))
+console.log(mergeNumberString("Age:",22))
 
 //Question: 65; 
 function Remainder(num1:number,num2:number):number {
     return num1 % num2
 }
-// console.log(Remainder(33,5))
-// console.log(Remainder(13,3))
-// console.log(Remainder(23,2))
+ console.log(Remainder(33,5))
+ console.log(Remainder(13,3))
+ console.log(Remainder(23,2))
 
 //Question:66;
 function checkBooleanValues(value1:boolean,value2:boolean) {
