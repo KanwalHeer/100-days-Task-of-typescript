4c11086af0e196db4f2263d711eec08cef864d5e
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-03-23 03:29:00 +0500

    Add files via upload

---
+++ 22day.ts
@@ -0,0 +1,23 @@
+//Question: 64;
+
+function mergeNumberString(strings:string,numbers:number) {
+    return strings + numbers
+}
+//console.log(mergeNumberString("Age:",22))
+
+//Question: 65; 
+function Remainder(num1:number,num2:number):number {
+    return num1 % num2
+}
+// console.log(Remainder(33,5))
+// console.log(Remainder(13,3))
+// console.log(Remainder(23,2))
+
+//Question:66;
+function checkBooleanValues(value1:boolean,value2:boolean) {
+    return value1 && value2
+}
+console.log(checkBooleanValues(true,false))
+console.log(checkBooleanValues(true,true))
+console.log(checkBooleanValues(false,false))
+console.log(checkBooleanValues(false,true))
