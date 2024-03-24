9fe43fce5813d3dcd6245686ac85fb94d04d646b
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-03-25 03:12:50 +0500

    Add files via upload

---
+++ 24day.ts
@@ -0,0 +1,53 @@
+//Question: 70;
+function LooP() {
+    for (let num = 1; num < 6 ; num++) {
+        console.log(num)
+    }
+}
+LooP()
+
+
+
+//Question:71;
+//let variable
+    let myAge = 22
+    console.log(myAge)
+    myAge = 23
+    console.log(myAge)
+// const variable
+// const myName = "kanwal"
+// try {
+//     myName = "Heer"
+// } catch (error) {
+//     console.log("Error; const variable can not reassigned")
+// }
+
+
+//Question: 72;
+
+//block scop   
+  {
+    let age = 22
+    const nameIs = "Kanwal"
+    var address = "karachi"
+    console.log(age)
+    console.log(nameIs)
+  } // console.log(age)
+    // console.log(nameIs)
+   
+    console.log(address)
+
+
+//global scop
+    let yourage = 23
+    const yournameIs = "Heer"
+    {
+        console.log(yourage)
+        console.log(yournameIs)
+    }
+    console.log(yourage)
+    console.log(yournameIs)
+    
+
+    
+
