6ebc0dc9b9ef19ca4bc1b82d486f9a2f3018b0a9
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-03-30 04:00:54 +0500

    Add files via upload

---
+++ 29day.ts
@@ -0,0 +1,22 @@
+//Question :85;
+function findPosition(str:string):number {
+    
+   return  str.indexOf("code")
+}
+//console.log(findPosition("you can see my code at my github"))
+
+
+//Question : 86;
+function findPresence(str1:string):boolean {
+    
+    return  str1.includes("typescript")
+ }
+ //console.log(findPresence("you can see my typescript code at my github"))
+
+ //Question: 87;
+
+ function findextracting(str1:string) {
+    
+    return  str1.substring(0,10)
+ }
+ console.log(findextracting("you can see my typescript code at my github"))
