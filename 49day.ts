b3b5d61dcb628c6cb95cf09d3f1bd821e6057604
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-04-20 14:25:53 -0700

    Update 49day.ts

--- 49day.ts
+++ 49day.ts
@@ -1,48 +1,44 @@
 //Question:145;
-// // This function accepts a callback function and invokes it with given arguments
-// function executeCallback( callback: (arg1: number, arg2: number) => void, arg1: number, arg2: number ): void {
-//     callback(arg1, arg2); 
-// }
-//   // Example callback function that adds two numbers
-//   const add = (a: number, b: number) => {
-//     console.log(a + b); 
-//   };
-//   executeCallback(add, 4, 3); 
+// This function accepts a callback function and invokes it with given arguments
+function executeCallback( callback: (arg1: number, arg2: number) => void, arg1: number, arg2: number ): void {
+    callback(arg1, arg2); 
+}
+  // Example callback function that adds two numbers
+  const add = (a: number, b: number) => {
+    console.log(a + b); 
+  };
+  executeCallback(add, 4, 3); 
 
 //Question:146;
 
-// const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
-// const filteredNumbers: number[] = numbers.filter((num) => num > 4);
+const filteredNumbers: number[] = numbers.filter((num) => num > 4);
 
-// console.log(filteredNumbers); 
+console.log(filteredNumbers); 
 
 
 //Question:147;
-// function fetchData(callback: (error: Error | null, data?: string) => void): void {
-//     const error = new Error("Failed to fetch data");
-//     const data = "Some data";
+function fetchData(callback: (error: Error | null, data?: string) => void): void {
+    const error = new Error("Failed to fetch data");
+    const data = "Some data";
     
-//     if (Math.random() > 0.5) {
-//       callback(null, data); 
-//     } else {
-//       callback(error); 
-//     }
-//   }
-//   const callback = (error, data) => {
-//     if (error) {
-//       console.error(error.message); 
-//     } else {
-//       console.log(data); 
-//     }};
-// fetchData(callback)
+    if (Math.random() > 0.5) {
+      callback(null, data); 
+    } else {
+      callback(error); 
+    }
+  }
+  const callback = (error, data) => {
+    if (error) {
+      console.error(error.message); 
+    } else {
+      console.log(data); 
+    }};
+fetchData(callback)
 
-if (!( true && false)) {
-  console.log("hello")
-}else{
-  console.log("bye")
-}
+
 
 
 
